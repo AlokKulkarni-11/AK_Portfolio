8f05dcb06111d753cd5daaa4afa734e2114ca90e
Alok <alok.kulkarni2k6@gmail.com>
2025-08-25 11:22:33 +0530

    feat: reduce typewriter font size

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.2 }}
-          className="text-2xl"
+          className="text-xl"
         >
         <Typewriter
           words={[
